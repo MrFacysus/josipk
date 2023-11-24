6202e2323f59da86371105886a95e12ae4708f5f
Mateo <noobzy99@gmail.com>
2023-11-24 22:05:33 +0100

    Updates

---
+++ precache-manifest.0b4625c4dac213340e20090091ce83d5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd2416e9d920e5d72bdd5c97b191d73",
+    "url": "/josipk/index.html"
+  },
+  {
+    "revision": "b0224993171134d7e95b",
+    "url": "/josipk/static/css/main.9d26880a.chunk.css"
+  },
+  {
+    "revision": "204020eb4ad2a8b89549",
+    "url": "/josipk/static/js/2.5f9d921b.chunk.js"
+  },
+  {
+    "revision": "b0224993171134d7e95b",
+    "url": "/josipk/static/js/main.95bd4feb.chunk.js"
+  },
+  {
+    "revision": "f6b81e0c2c1ddc8aec24",
+    "url": "/josipk/static/js/runtime~main.b3862f4e.js"
+  },
+  {
+    "revision": "424a2d256de1d2a18379746a7a2b7f3f",
+    "url": "/josipk/static/media/eighthpost.424a2d25.jpg"
+  },
+  {
+    "revision": "a37f1832ac4846982090e699edf374a8",
+    "url": "/josipk/static/media/eighthpost.a37f1832.png"
+  },
+  {
+    "revision": "89d9f1e44688ab0c17bfedd9c469eccb",
+    "url": "/josipk/static/media/fifthpost.89d9f1e4.png"
+  },
+  {
+    "revision": "a864410cb5b81f9a54f54a8e111defe2",
+    "url": "/josipk/static/media/fifthpost.a864410c.jpg"
+  },
+  {
+    "revision": "f5f710a4c60f5c7eff9ebf325b35d716",
+    "url": "/josipk/static/media/firstpost.f5f710a4.png"
+  },
+  {
+    "revision": "fecf2290ae68a2be72b7021ec94987ae",
+    "url": "/josipk/static/media/firstpost.fecf2290.jpg"
+  },
+  {
+    "revision": "27dc29712aba44ec7cb5352ff145e9b0",
+    "url": "/josipk/static/media/fourthpost.27dc2971.jpg"
+  },
+  {
+    "revision": "70f13f9c24334b9808a5618f331f6d45",
+    "url": "/josipk/static/media/fourthpost.70f13f9c.png"
+  },
+  {
+    "revision": "396937a00039dd3ffb50fc9efaae95a6",
+    "url": "/josipk/static/media/god.396937a0.mp4"
+  },
+  {
+    "revision": "8c95e7495ef35161cd0ec401c55ccc6f",
+    "url": "/josipk/static/media/ninethpost.8c95e749.png"
+  },
+  {
+    "revision": "f2f8d8ec2669440ea01bf69b061ad77c",
+    "url": "/josipk/static/media/ninethpost.f2f8d8ec.png"
+  },
+  {
+    "revision": "416909e6f58577cdfdd585cbbb2b23fb",
+    "url": "/josipk/static/media/secondpost.416909e6.png"
+  },
+  {
+    "revision": "fbb3d2b5f5d4b5f6cdc798b820d2ace0",
+    "url": "/josipk/static/media/secondpost.fbb3d2b5.jpg"
+  },
+  {
+    "revision": "512e1d9ffbcb4496fbb5d4c3c6df844f",
+    "url": "/josipk/static/media/seventhpost.512e1d9f.jpg"
+  },
+  {
+    "revision": "8bc3a9a41882f9a93e11b8d16d748a6e",
+    "url": "/josipk/static/media/seventhpost.8bc3a9a4.png"
+  },
+  {
+    "revision": "d6aa19ad726c1074a6646554e8f77edb",
+    "url": "/josipk/static/media/sixthpost.d6aa19ad.jpg"
+  },
+  {
+    "revision": "e011eb6f3cc1b2488fee578faab4b937",
+    "url": "/josipk/static/media/sixthpost.e011eb6f.png"
+  },
+  {
+    "revision": "112149b0649176ca74eba8a1b1847bad",
+    "url": "/josipk/static/media/tenthpost.112149b0.jpg"
+  },
+  {
+    "revision": "37be0afdb643a4939d33242ac6765d6a",
+    "url": "/josipk/static/media/tenthpost.37be0afd.png"
+  },
+  {
+    "revision": "837c75336de3be0fb9d49402b07caf4b",
+    "url": "/josipk/static/media/thirdpost.837c7533.png"
+  },
+  {
+    "revision": "fad3a872ffe293511cbc6c8937373a17",
+    "url": "/josipk/static/media/thirdpost.fad3a872.jpg"
+  }
+]);
